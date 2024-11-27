ba0674942592f6f8138f00fb71a4cd0279fec0e2
NovMarfa <mn211195@gmail.com>
2024-11-27 20:28:36 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,12 @@
+immutable_var = 1, 'shirt', False, 5.03
+print (immutable_var)
+print (type (immutable_var))
+# immutable_var [0] = 7    # это неизменяемая коллекция данных, в отличие от списка, чаще используется как хранилище
+
+mutable_list = [3, 'potato', 17.5, 'mango']
+print (mutable_list)
+mutable_list [0] = False
+mutable_list [1] = 'bbb'
+mutable_list [2] = 'PiKa'
+mutable_list [3] = 146.8
+print (mutable_list)
